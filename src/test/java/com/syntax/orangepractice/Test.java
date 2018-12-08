441c2dfe979a8a2663e1c7e86c6e8d612d82c147
TannazNazar <tannaznazar@Tannazs-MacBook-Air.local>
2018-12-08 12:24:07 -0500

    first init

---
+++ src/test/java/com/syntax/orangepractice/Test.java
@@ -0,0 +1,11 @@
+package com.syntax.orangepractice;
+
+public class Test {
+
+	@org.testng.annotations.Test
+	public void basic1() {
+	System.out.println("Hello from test");
+}
+
+}
+
---
+++ src/test/java/com/syntax/orangepractice/Test1.java
@@ -0,0 +1,12 @@
+package com.syntax.orangepractice;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void basic() {
+	System.out.println("Hello from the Test1");
+}
+}
